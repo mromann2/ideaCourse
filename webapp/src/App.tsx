79f7859001cda918d6fd2e1f627ed81414a9ae4e
medjidov <mromann@inbox.ru>
2025-03-07 17:29:51 +0300

    New message fixed2

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 import { TrpcProvider } from "./lib/trpc";
 import { AllIdeas } from "./pages/AllIdeasPage/AllIdeas";
 
-export const App = () => {
+export function App() {
   return (
     <TrpcProvider>
       <AllIdeas />
     </TrpcProvider>
   );
-};
+}
